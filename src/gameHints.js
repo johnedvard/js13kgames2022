6b97cd840b598d6972c8713e8158a7a93a166400
John Edvard Reiten <johnedvard89@gmail.com>
2022-11-10 20:08:43 +0900

    refactor: adjust levels and hints

--- src/gameHints.js
+++ src/gameHints.js
@@ -1,17 +1,16 @@
 export const initGameHints = (levelId) => {
-  const txt = '\nCut the rope with "S".';
   const hintEl = document.getElementById('hint');
   hintEl.textContent = '';
   switch (levelId) {
     case 1:
-      hintEl.textContent =
-        'Reach the goal below.\nUse the arrow keys to move.' + txt;
+      hintEl.textContent = 'Reach the goal below. \nCut the rope with "S".';
       break;
     case 2:
-      hintEl.textContent = 'Use "Up Arrow" to shorten the rope';
+      hintEl.textContent =
+        'Use the arrow keys to move.\n Use "Space" to boost up.';
       break;
     case 3:
-      hintEl.textContent = 'Use "Space" to boost up.' + txt;
+      hintEl.textContent = '';
       break;
   }
 };
