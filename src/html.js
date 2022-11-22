e7ef336d72325b84f7cdc2a65fa62d891e5c0451
John Edvard Reiten <johnedvard89@gmail.com>
2022-11-22 21:30:34 +0900

    refactor: init html in game and remove dead code

--- src/html.js
+++ src/html.js
@@ -1,1 +1,60 @@
-export const initHtml = () => {};
+export const initHtml = () => {
+  const html = `
+  <div id="wrap">
+      <div id="loading-wrapper" class="overlay hide">
+        <div id="loading"></div>
+      </div>
+      <canvas id="game-canvas" width="800px" height="800px"> </canvas>
+      <div id="r-menu" class="menu">
+        <div id="hamburger">––</div>
+      </div>
+      <menu id="main" class="overlay">
+        <button id="select-level-btn">Select Level</button>
+        <!-- <button id="near-login-loading-btn">Loading..</button>
+        <button id="near-level-btn" class="disabled" disabled>
+          NEAR Levels
+        </button> -->
+        <!-- <button id="bonus-content-btn">Bonus Content</button> -->
+        <button id="music-btn" class="flat">Music is OFF</button>
+      </menu>
+      <div id="bonus" class="overlay hide">
+        <div id="coil-subscriber" class="hide">
+          Thanks for subscribing to Coil
+        </div>
+
+        <span id="coil-btn">Access more hats for Coil subscribers</span>
+
+        <div class="bone">
+          🦴
+          <div class="inverse">🦴</div>
+        </div>
+        <div id="bonus-grid"></div>
+      </div>
+      <div id="levels" class="overlay hide">
+        <div class="bone">
+          🦴
+          <div class="inverse">🦴</div>
+        </div>
+        <div id="levels-grid"></div>
+      </div>
+      <div id="near-levels" class="overlay hide">
+        <div class="bone">
+          🦴
+          <div class="inverse">🦴</div>
+        </div>
+        <div id="loading-near-levels">Loading NEAR levels..</div>
+        <div id="near-levels-grid"></div>
+      </div>
+      <menu id="level-dialog" class="overlay hide">
+        <button id="next-btn">Play next level</button>
+        <button id="replay-btn">Replay level</button>
+      </menu>
+      <div id="thanks" class="overlay hide">
+        Rest in peace my soul
+        <br />
+        Thank you for playing
+      </div>
+      <div id="hint"></div>
+    </div>`;
+  document.getElementById('hang-by-a-thread').innerHTML = html;
+};
--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
 import cssText from 'bundle-text:./styles.css';
 
 import { Game } from './Game';
-import { initLoginLogout } from './near/nearLogin';
-import {
-  HANG_BY_A_THREAD_SERIES_TESTNET,
-  NearConnection,
-} from './near/nearConnection';
 import { initMenu } from './menu';
-import { setNftTokens } from './store';
 import { initMonetization } from './monetization';
-import { loadScript } from './utils';
 import { initMenuControls } from './menuControls';
 import { initHtml } from './html';
 
 export const initGame = ({ deathCount, setDeathCount }) => {
+  initHtml();
   addStyles();
   new Game({ deathCount, setDeathCount });
   // TODO (johnedvard) add build flag to prevent adding NEAR if we build for crazy games
   // initNear();
-  initHtml();
   initMenu();
   initMenuControls();
   initMonetization();
@@ -30,29 +23,4 @@ const addStyles = () => {
   style.textContent = cssText;
   const headEl = document.getElementsByTagName('head')[0];
   headEl.appendChild(style);
-};
-
-const initNear = () => {
-  loadNearApi().then(() => {
-    const nearConnection = new NearConnection();
-    nearConnection.initContract().then(() => {
-      initLoginLogout(nearConnection);
-      if (!nearConnection.isSignedIn()) return;
-      const promises = [
-        nearConnection.nft_tokens_for_owner(nearConnection.accountId),
-        nearConnection.nft_tokens_by_series(HANG_BY_A_THREAD_SERIES_TESTNET),
-        nearConnection.getNftCollections(),
-      ];
-
-      Promise.all(promises).then(
-        ([tokensForOwner, tokensBySeries, collections]) => {
-          setNftTokens(tokensForOwner, tokensBySeries, collections);
-        }
-      );
-    });
-  });
-};
-
-const loadNearApi = () => {
-  return loadScript('https://js13kgames.com/src/near-api-js.js');
 };
