b0ee0543c20d87e5842e1540dd91ef4600dadce3
John Edvard Reiten <johnedvard89@gmail.com>
2023-01-01 22:11:56 +0900

    feat: add crazygames ad banner

--- src/crazyGames.js
+++ src/crazyGames.js
@@ -3,6 +3,19 @@ import { AD_FINISHED, AD_PLAYING } from './gameEvents';
 import { loadScript } from './utils';
 
 let crazyGameSdk;
+const crazyAdSizes = [
+  { x: 970, y: 90 },
+  { x: 320, y: 50 },
+  { x: 160, y: 600 },
+  { x: 336, y: 280 },
+  { x: 728, y: 90 },
+  { x: 300, y: 600 },
+  { x: 468, y: 60 },
+  { x: 970, y: 250 },
+  { x: 300, y: 250 },
+  { x: 250, y: 250 },
+  { x: 120, y: 600 },
+];
 
 export const initCrazyGamesSdk = () => {
   if (!location.href.match('crazygames') || location.href.match('netlify')) {
@@ -12,6 +25,7 @@ export const initCrazyGamesSdk = () => {
     crazyGameSdk = window.CrazyGames.CrazySDK.getInstance();
     crazyGameSdk.init();
     listenForAdEvents();
+    initAdBanner();
     return crazyGameSdk;
   });
 };
@@ -34,6 +48,38 @@ const listenForAdEvents = () => {
   crazyGameSdk?.addEventListener('adStarted', adStarted);
   crazyGameSdk?.addEventListener('adFinished', adFinished);
   crazyGameSdk?.addEventListener('adError', adError);
+};
+
+const initAdBanner = () => {
+  const adBannerEl = document.createElement('div');
+  adBannerEl?.setAttribute('id', 'responsive-banner-container');
+  const { width, height } = getLargestCrazyAd();
+
+  adBannerEl?.setAttribute('style', `width: ${width}px; height: ${height}px`);
+  document.querySelector('body').appendChild(adBannerEl);
+  crazyGameSdk?.requestResponsiveBanner(['responsive-banner-container']);
+};
+
+const getLargestCrazyAd = () => {
+  const gameContainerEl = document.querySelector('#hang-by-a-thread');
+  const availableWidth = window.innerWidth - gameContainerEl.clientWidth;
+  const availableHeight = window.innerHeight;
+  let bestWidth = 0;
+  let bestHeight = 0;
+  let area = 0;
+  crazyAdSizes.forEach(({ x, y }) => {
+    if (
+      x >= bestWidth &&
+      x <= availableWidth &&
+      y <= availableHeight &&
+      y * x >= area
+    ) {
+      bestWidth = x;
+      bestHeight = y;
+      area = x * y;
+    }
+  });
+  return { height: bestHeight, width: bestWidth };
 };
 
 export const happytime = () => {
