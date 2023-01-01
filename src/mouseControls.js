0a57a3460574a1553ba8a400ed3c328060d46fc9
John Edvard Reiten <johnedvard89@gmail.com>
2023-01-01 21:50:50 +0900

    feat: add mouse pointer controls

--- src/mouseControls.js
+++ src/mouseControls.js
@@ -1,5 +1,6 @@
-import { getPointer } from 'kontra';
+import { getPointer, pointerPressed } from 'kontra';
 import { fgc2 } from './constants';
+import { gameHeight, gameWidth } from './store';
 
 let isDragging = false;
 export const mousePoints = [];
@@ -59,4 +60,21 @@ export const drawMouseLine = (ctx) => {
     prev.draws--;
   }
   removeMouseDraggings();
+};
+
+export const updateCanvasMouseArea = (player) => {
+  if (pointerPressed('left')) {
+    const pointer = getPointer();
+    if (pointer.y > gameHeight - gameHeight / 5) {
+      player.applyForce(0, -5);
+    }
+    if (pointer.x > gameWidth - gameWidth / 5) {
+      player.applyForce(1.5, -1);
+      player.changePlayerDirection(false);
+    }
+    if (pointer.x <= gameWidth / 5) {
+      player.applyForce(-1.5, -1);
+      player.changePlayerDirection(true);
+    }
+  }
 };
